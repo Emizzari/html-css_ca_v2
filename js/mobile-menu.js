141f73ac7143ccc933a4f98059578f518a461743
Emizzari <emilie@tonnessen.com>
2020-05-29 13:40:17 +0200

    Mobile scroll
    Adding the feature where the mobile nav bar will disepear upon scrolling downwards and appear when scrolling upwards

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,14 @@
-function mobileMenuToggle(){
+// Mobile Menu Hide on scolling down - show on scrolling up
+let scroll = window.pageYOffset;
 
+window.onscroll = function () {
+    const currentScroll = window.pageYOffset;
+
+    if (scroll > currentScroll) {
+        document.querySelector(".nav").style.bottom = "0";
+    } else {
+        document.querySelector(".nav").style.bottom = "-100px";
+    }
+
+    scroll = currentScroll;
 }
-
